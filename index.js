653cc3ac59fa89e51ce323d1d7d2bfa17384d501
Eric <eric.m.lombardo@gmail.com>
2021-06-11 14:46:38 -0400

    commit tree 767fd870e186a93b78a30fb925438ecb4aed941a

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers){
+  return drivers.slice(0,2)
+}
+
+const returnLastTwoDrivers = function(drivers){
+  return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = function(multiplier){
+  return function(value) {
+    return multiplier * value
+  }
+}
+
+const fareDoubler = createFareMultiplier(2)
+// const fareDoubler = function(fare){      originally had it this way until seeing you could just call
+//   return createFareMultiplier(2)(fare)   the function after the assignment. (see above)
+// }
+
+const fareTripler = createFareMultiplier(3)
+
+const selectDifferentDrivers = function(arrayOfDrivers, driverSelector){
+  return driverSelector(arrayOfDrivers)
+}
